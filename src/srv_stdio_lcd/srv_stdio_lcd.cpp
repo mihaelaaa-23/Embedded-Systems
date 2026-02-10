ad78088df8e30aba10e5864fec83ea5f7ba108ef
Mihaela <mihaela.untu@gmail.com>
2026-02-10 22:32:14 +0200

    Add LCD service implementation with setup and character output functions

---
+++ src/srv_stdio_lcd/srv_stdio_lcd.cpp
@@ -0,0 +1,28 @@
+#include "srv_stdio_lcd.h"
+#include <stdio.h>
+#include <LiquidCrystal_I2C.h>
+
+#define CLEAR_KEY 0x1b
+
+int lcdColumns = 16;
+int lcdRows = 2;
+
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+int srv_stdio_lcd_put_char(char c, FILE *stream) {
+    if (c == CLEAR_KEY){
+        lcd.clear();
+        lcd.setCursor(0, 0);
+    } else {
+
+    }
+    lcd.print(c);
+}
+
+void srv_stdio_lcd_setup() {
+    lcd.begin(lcdColumns, lcdRows);
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    FILE *srv_stdio_lcd_stream = fdevopen(srv_stdio_lcd_put_char, NULL);
+    stdout = srv_stdio_lcd_stream;
+} 
---
+++ src/srv_stdio_lcd/srv_stdio_lcd.h
@@ -0,0 +1,6 @@
+#ifndef SRV_STDIO_LCD_H
+#define SRV_STDIO_LCD_H
+
+void srv_stdio_lcd_setup();
+
+#endif // SRV_STDIO_LCD_H
