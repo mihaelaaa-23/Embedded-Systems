40c33d8b49f5dad9a95e184f43276cf823cd477c
Mihaela <mihaela.untu@gmail.com>
2026-02-10 22:32:04 +0200

    Add keypad functionality with setup and key reading methods

---
+++ src/srv_stdio_keypad/srv_stdio_keypad.cpp
@@ -0,0 +1,57 @@
+#include "srv_stdio_keypad.h"
+
+//1. Includere STDIO
+#include <stdio.h>
+//2. Includere Keypad
+#include <Keypad.h>
+
+// Configurare Keypad
+#define SRV_KEYPAD_REPEAT_DELAY 100
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {3, 2, 1, 0}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+//3. Functie pentru citirea unei taste de la Keypad
+int srv_stdio_keypad_get_key(FILE *stream) {
+    char customKey;
+    do {
+        customKey = customKeypad.getKey();
+    } while (!customKey);
+
+    delay(SRV_KEYPAD_REPEAT_DELAY); // read frequency 1/SRV_KEYPAD_REPEAT_DELAY
+
+    return customKey;   
+}
+
+void srv_stdio_keypad_setup() {
+  //4. initializare periferii
+  // nu necesita, e initializat prin configuratia de definitie, vezi mai sus
+
+  //5. Definire stream
+  FILE *my_stream = fdevopen(NULL, srv_stdio_keypad_get_key);
+
+  // link stream to keypad
+    stdin = my_stream;
+} 
+
+
+
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
+}
---
+++ src/srv_stdio_keypad/srv_stdio_keypad.h
@@ -0,0 +1,7 @@
+#ifndef SRV_STDIO_KEYPAD_H
+#define SRV_STDIO_KEYPAD_H
+
+void srv_stdio_keypad_setup();
+char srv_stdio_keypad_get_key();
+
+#endif // SRV_STDIO_KEYPAD_H
