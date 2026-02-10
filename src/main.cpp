0847995c5729f1d1b7aa2b3ad2eff5c7e1a64769
Mihaela <mihaela.untu@gmail.com>
2026-02-10 22:32:35 +0200

    Switch to app_lab_1_2 for setup and loop functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,22 @@
 #include "app_lab_1_1/app_lab_1_1.h"
 
 #define USE_APP_LAB_1_1 11
-#define APP_USED USE_APP_LAB_1_1
+#define USE_APP_LAB_1_2 12
+
+#define APP_USED USE_APP_LAB_1_2
 
 void setup() {
   #if APP_USED == USE_APP_LAB_1_1
     app_lab_1_1_setup();
+  #elif APP_USED == USE_APP_LAB_1_2
+    app_lab_1_2_setup();
   #endif
 }
 
 void loop() {
   #if APP_USED == USE_APP_LAB_1_1
     app_lab_1_1_loop();
+  #elif APP_USED == USE_APP_LAB_1_2
+    app_lab_1_2_loop();
   #endif
 }
