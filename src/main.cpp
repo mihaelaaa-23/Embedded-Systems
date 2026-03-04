edfd4f029675f3f7c9fc4debe97b2530bf3706fc
Mihaela <mihaela.untu@gmail.com>
2026-03-04 13:23:47 +0200

    Update application to use Lab 4.1 with setup and loop functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 #include "app_lab_2_1/app_lab_2_1.h"
 #include "app_lab_3_1/app_lab_3_1.h"
 #include "app_lab_3_2/app_lab_3_2.h"
+#include "app_lab_4_1/app_lab_4_1.h"
 #define USE_APP_LAB_1_1 11
 #define USE_APP_LAB_1_2 12
 #define USE_APP_LAB_2_1 21
 #define USE_APP_LAB_3_1 31
 #define USE_APP_LAB_3_2 32
+#define USE_APP_LAB_4_1 41
 
-#define APP_USED USE_APP_LAB_3_2
+#define APP_USED USE_APP_LAB_4_1
 
 void setup() {
   #if APP_USED == USE_APP_LAB_1_1
@@ -23,6 +25,8 @@ void setup() {
     app_lab_3_1_setup();
   #elif APP_USED == USE_APP_LAB_3_2
     app_lab_3_2_setup();
+  #elif APP_USED == USE_APP_LAB_4_1
+    app_lab_4_1_setup();
   #endif
 }
 
@@ -37,5 +41,7 @@ void loop() {
     app_lab_3_1_loop();
   #elif APP_USED == USE_APP_LAB_3_2
     app_lab_3_2_loop();
+  #elif APP_USED == USE_APP_LAB_4_1
+    app_lab_4_1_loop();
   #endif
 }
