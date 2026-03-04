6690d52cdf4e38fc7713f8ba37636a2f33fe59ae
Mihaela <mihaela.untu@gmail.com>
2026-03-04 13:23:35 +0200

    Refactor srv_stdio_lcd_put_char to track cursor position and handle new line characters

--- src/srv_stdio_lcd/srv_stdio_lcd.cpp
+++ src/srv_stdio_lcd/srv_stdio_lcd.cpp
@@ -10,12 +10,27 @@ int lcdRows = 2;
 
 LiquidCrystal_I2C lcd(0x3F, lcdColumns, lcdRows);
 
+static int s_lcd_row = 0;  // tracks current LCD cursor row
+
 int srv_stdio_lcd_put_char(char c, FILE *stream) {
-    if (c == CLEAR_KEY){
+    if (c == CLEAR_KEY) {
         lcd.clear();
         lcd.setCursor(0, 0);
+        s_lcd_row = 0;
+    } else if (c == '\n') {
+        s_lcd_row++;
+        if (s_lcd_row < lcdRows) {
+            lcd.setCursor(0, s_lcd_row);
+        }
+        Serial.write('\r');
+        Serial.write('\n');
+    } else if (c == '\r') {
+        Serial.write('\r');          // \r: Serial only, skip LCD
     } else {
-        lcd.print(c);
+        if (s_lcd_row < lcdRows) {   // rows 0-1 visible on LCD
+            lcd.print(c);
+        }
+        Serial.write(c);             // all chars tee'd to Serial
     }
     return 0;
 }
